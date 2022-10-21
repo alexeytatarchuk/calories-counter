8caf0c141628d5fadf4978d7bc4355b77e13c92e
Tatarchuk Alexey <alexey.tatarchuk@gmail.com>
2022-10-22 01:57:02 +0300

    Added result and format functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Counter from './modules/counter.js';
 
-const counterElements = document.querySelectorAll(".counter");
+const counterElements = document.querySelectorAll(`.counter`);
 
 counterElements.forEach((element) => {
     const counter = new Counter(element);
