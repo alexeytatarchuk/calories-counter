dde32498ba7addb664e9ea8227134957895fdaa2
Ritermargariter <112171327+Ritermargariter@users.noreply.github.com>
2022-11-18 15:40:43 +0300

    Minor fix

--- js/utils/format-input.js
+++ js/utils/format-input.js
@@ -3,5 +3,5 @@ const NOT_NUMBERS = /[^\d]/g;
 
 
 export const formatInput = (input) => {
-    return input.value.replace(NOT_NUMBERS, ``).replace(LEAD_ZERO, ``);
+    return input.value.replace(NOT_NUMBERS, '').replace(LEAD_ZERO, '');
 };
